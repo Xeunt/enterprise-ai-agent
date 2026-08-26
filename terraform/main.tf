5cff092eb16758f78c1a32eb94c6be3e4b694de6
Chad <jcbpabalan@gmail.com>
2026-08-26 21:56:05 +0800

    Add backend tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket = "enterprise-ai-document-agent-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
